61e3dabec0bdcde592132556c8f6ba5e8c1215a8
KelvinKass <147566640+KelvinKass@users.noreply.github.com>
2023-10-10 21:19:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.querySelector("h1").textContent="Programação e Sistemas de Informação";
+document.querySelector("footer").innerHTML+=`<ahref="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a>`;
